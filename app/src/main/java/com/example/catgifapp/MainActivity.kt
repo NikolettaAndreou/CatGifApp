c81226de591518aead1963b5e1a25ffa4181e071
Nikoletta Andreou <andreou_nikoletta@hotmail.com>
2026-04-07 14:55:00 +0300

    Test fetching of 10 gifs

--- app/src/main/java/com/example/catgifapp/MainActivity.kt
+++ app/src/main/java/com/example/catgifapp/MainActivity.kt
@@ -3,45 +3,36 @@ package com.example.catgifapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.catgifapp.ui.theme.CatGifAppTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            CatGifAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+            var cats by remember { mutableStateOf(listOf<CatResponse>()) }
+
+            LaunchedEffect(Unit) {
+                try {
+                    cats = RetrofitInstance.api.getCats()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            LazyColumn {
+                items(cats) { cat ->
+                    AsyncImage(
+                        model = cat.url,
+                        contentDescription = null
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CatGifAppTheme {
-        Greeting("Android")
     }
 }
